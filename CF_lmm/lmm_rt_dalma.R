73b571fecadbe2dfa36b20e6b27537a2aab390ce
Haiyang <hjin317@aucklanduni.ac.nz>
2020-11-06 17:09:50 +0400

    update analyses for RT (for all trials; not only correct trials)

--- CF_lmm/lmm_rt_dalma.R
+++ CF_lmm/lmm_rt_dalma.R
@@ -11,11 +11,10 @@ load("df_lmm.RData")
 ######### zcp model #########
 # message("Fitting glmm_rt_zcp...")
 # glmm_rt_zcp <- lmer(
-#     log(RT) ~ Cue * Congruency * Alignment * SameDifferent + Probability + 
-#         (Cue_C + Con_C + Ali_C + Sam_C + 
-#              Cue_Con + Cue_Ali + Cue_Sam + Con_Ali + Con_Sam + Ali_Sam +
-#              Cue_Con_Ali + Cue_Con_Sam + Cue_Ali_Sam + Con_Ali_Sam + 
-#              Cue_Con_Ali_Sam || Participant),
+#     log(RT) ~ Cue * Congruency * Alignment + Probability + 
+#         (Cue_C + Con_C + Ali_C + 
+#              Cue_Con + Cue_Ali + Con_Ali + 
+#              Cue_Con_Ali || Participant),
 #     data = df_lmm,
 #     control = lmerControl(optimizer = "optimx", # calc.derivs = FALSE,
 #                           optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))
@@ -24,14 +23,13 @@ load("df_lmm.RData")
 # message("zcp for rt is finished.")
 
 
-######### rdc model #########
+# ######### rdc model (not used) #########
 # message("Fitting glmm_rt_rdc...")
 # glmm_rt_rdc <- lmer(
-#     log(RT) ~ Cue * Congruency * Alignment * SameDifferent + Probability + 
-#         (Cue_C + Con_C + Ali_C + Sam_C + 
-#              Cue_Con + Cue_Ali + Cue_Sam + Ali_Sam + Con_Sam + # Con_Ali + 
-#              Cue_Con_Ali + Cue_Ali_Sam  # Cue_Con_Sam + + Con_Ali_Sam +
-#          || Participant), # Cue_Con_Ali_Sam
+#     log(RT) ~ Cue * Congruency * Alignment + Probability + 
+#         (Cue_C + Con_C + Ali_C + 
+#              Cue_Con + Con_Ali + # Cue_Ali + 
+#              Cue_Con_Ali || Participant),
 #     data = df_lmm,
 #     control = lmerControl(optimizer = "optimx", # calc.derivs = FALSE,
 #                           optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))
@@ -43,11 +41,10 @@ load("df_lmm.RData")
 ######### etd model #########
 # message("Fitting glmm_rt_etd...")
 # glmm_rt_etd <- lmer(
-#     log(RT) ~ Cue * Congruency * Alignment * SameDifferent + Probability + 
-#         (Cue_C + Con_C + Ali_C + Sam_C + 
-#              Cue_Con + Cue_Ali + Cue_Sam + Ali_Sam + Con_Sam + # Con_Ali + 
-#              Cue_Con_Ali + Cue_Ali_Sam  # Cue_Con_Sam + + Con_Ali_Sam +
-#          | Participant), # Cue_Con_Ali_Sam
+#     log(RT) ~ Cue * Congruency * Alignment + Probability + 
+#         (Cue_C + Con_C + Ali_C + 
+#              Cue_Con + Cue_Ali + Con_Ali + 
+#              Cue_Con_Ali | Participant),
 #     data = df_lmm,
 #     control = lmerControl(optimizer = "optimx", # calc.derivs = FALSE,
 #                           optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))
@@ -59,11 +56,10 @@ load("df_lmm.RData")
 ######### etd1 model #########
 # message("Fitting glmm_rt_etd1...")
 # glmm_rt_etd1 <- lmer(
-#     log(RT) ~ Cue * Congruency * Alignment * SameDifferent + Probability + 
-#         (Cue_C + Sam_C + # Con_C + Ali_C + 
-#              Cue_Ali + Con_Sam + # Cue_Con + Con_Ali + Ali_Sam + Cue_Sam +
-#              Cue_Con_Ali + Cue_Ali_Sam  # Cue_Con_Sam + Con_Ali_Sam +
-#          | Participant), # Cue_Con_Ali_Sam
+#     log(RT) ~ Cue * Congruency * Alignment + Probability + 
+#         (Cue_C +  # Con_C + Ali_C +
+#              Cue_Ali + # Con_Ali + Cue_Con +
+#              Cue_Con_Ali | Participant),
 #     data = df_lmm,
 #     control = lmerControl(optimizer = "optimx", # calc.derivs = FALSE,
 #                           optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))
@@ -71,54 +67,9 @@ load("df_lmm.RData")
 # save(glmm_rt_etd1, file = "rt_lmm_etd1.RData")
 # message("etd for rt is finished.")
 
-
-######### etd2 model #########
-# message("Fitting glmm_rt_etd2...")
-# glmm_rt_etd2 <- lmer(
-#     log(RT) ~ Cue * Congruency * Alignment * SameDifferent + Probability + 
-#         (Cue_C +  # Con_C + Ali_C + Sam_C +
-#              # Cue_Ali + Cue_Con + Con_Ali + Ali_Sam + Cue_Sam + Con_Sam +
-#              Cue_Con_Ali + Cue_Ali_Sam  # Cue_Con_Sam + Con_Ali_Sam +
-#          | Participant), # Cue_Con_Ali_Sam
-#     data = df_lmm,
-#     control = lmerControl(optimizer = "optimx", # calc.derivs = FALSE,
-#                           optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))
-# )
-# save(glmm_rt_etd2, file = "rt_lmm_etd2.RData")
-# message("etd for rt is finished.")
-
-
-######### etd3 model #########
-# message("Fitting glmm_rt_etd3...")
-# glmm_rt_etd3 <- lmer(
-#     log(RT) ~ Cue * Congruency * Alignment * SameDifferent + Probability + 
-#         (Cue_C +  # Con_C + Ali_C + Sam_C +
-#              # Cue_Ali + Cue_Con + Con_Ali + Ali_Sam + Cue_Sam + Con_Sam +
-#              Cue_Con_Ali  # Cue_Con_Sam + Con_Ali_Sam + + Cue_Ali_Sam 
-#          | Participant), # Cue_Con_Ali_Sam
-#     data = df_lmm,
-#     control = lmerControl(optimizer = "optimx", # calc.derivs = FALSE,
-#                           optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))
-# )
-# save(glmm_rt_etd3, file = "rt_lmm_etd3.RData")
-# message("etd for rt is finished.")
-
-
-######### etd4 model #########
-message("Fitting glmm_rt_etd4...")
-glmm_rt_etd4 <- lmer(
-    log(RT) ~ Cue * Congruency * Alignment * SameDifferent + Probability + 
-        (Cue_C   # Con_C + Ali_C + Sam_C +
-         # Cue_Ali + Cue_Con + Con_Ali + Ali_Sam + Cue_Sam + Con_Sam +
-         # Cue_Con_Sam + Con_Ali_Sam + + Cue_Ali_Sam + Cue_Con_Ali
-         | Participant), # Cue_Con_Ali_Sam
-    data = df_lmm,
-    control = lmerControl(optimizer = "optimx", # calc.derivs = FALSE,
-                          optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))
-)
-save(glmm_rt_etd4, file = "rt_lmm_etd4.RData")
-message("etd for rt is finished.")
-
-
-
-
+######### emmeans #########
+# library(emmeans)
+# load("rt_lmm_zcp.RData")
+# glmm_rt_opt <- glmm_rt_zcp
+# emm_rt <- emmeans(glmm_rt_opt, ~ Cue + Congruency + Alignment)
+# save(emm_rt, file = "rt_emm.RData")
